1b4faecc3cde13f204b4084cc167f81d6f2db8b1
BugHunter-X <askzenai@gmail.com>
2025-01-28 09:26:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080); // This is the problem line.  It is missing an error handler.
